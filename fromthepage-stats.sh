367d243beed50dc0cd18e8c167aa8637130decb1
Ben Companjen <ben@companjen.name>
2024-10-14 18:06:45 +0200

    Run the stats cronjob with production Rails env

--- fromthepage-stats.sh
+++ fromthepage-stats.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 . /home/app/fromthepage/load-secrets-to-env.sh
-cd /home/app/fromthepage && /sbin/setuser app bundle exec rake fromthepage:email_stats[24] >> /home/app/fromthepage/log/cron.log 2>&1
+cd /home/app/fromthepage && /sbin/setuser app RAILS_ENV=production bundle exec rake fromthepage:email_stats[24] >> /home/app/fromthepage/log/cron.log 2>&1
