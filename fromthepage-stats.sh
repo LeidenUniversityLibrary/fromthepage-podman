ce05ab0eb3b8eba59a5924458276c34b126d7330
Ben Companjen <ben@companjen.name>
2024-10-15 16:24:54 +0200

    Log date/time of job start

--- fromthepage-stats.sh
+++ fromthepage-stats.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo "Starting email stats at $(date)" >> /home/app/fromthepage/log/cron.log
 . /home/app/fromthepage/load-secrets-to-env.sh
 cd /home/app/fromthepage && /sbin/setuser app bundle exec rake fromthepage:email_stats[24] >> /home/app/fromthepage/log/cron.log 2>&1
